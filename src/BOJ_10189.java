bf0072968a0cac7489c410c8dabfc9d0b281bf3e
kimgiraffe <kimgiraffe@naver.com>
2023-07-11 22:02:40 +0900

    Hook

---
+++ src/BOJ_10189.java
@@ -0,0 +1,8 @@
+class BOJ_10189 {
+	public static void main(String[] args) throws Exception {
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+	}
+}
