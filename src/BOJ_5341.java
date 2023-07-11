5c079051cedca92065808af10650ab20d80fd398
kimgiraffe <kimgiraffe@naver.com>
2023-07-11 21:59:58 +0900

    Pyramids

---
+++ src/BOJ_5341.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class BOJ_5341 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int n = sc.nextInt();
+			if(n == 0) break;
+			System.out.println(n * (n + 1) / 2);
+		}
+		
+		sc.close();
+	}
+}
