a9676502f9f5bcbc35ddc0304ae43ad0a55cd563
kimgiraffe <kimgiraffe@naver.com>
2023-07-11 22:34:25 +0900

    時間 (Hour)

---
+++ src/BOJ_27327.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class BOJ_27327 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		System.out.println(h * 24);
+		
+		sc.close();
+	}
+}
