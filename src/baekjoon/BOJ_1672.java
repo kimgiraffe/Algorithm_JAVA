f077217f07837fa5496d82c0beb003bb05c70443
kimgiraffe <kimgiraffe@naver.com>
2023-07-20 21:38:52 +0900

    DNA 해독

---
+++ src/baekjoon/BOJ_1672.java
@@ -0,0 +1,90 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/1672
+ * @author kimgiraffe
+ */
+public class BOJ_1672 {
+	
+	static int N;
+	static String DNA;
+	
+	private static char decode(char c1, char c2) {		
+		if(c1 == 'A') {
+			if(c2 == 'A') {
+				return 'A';
+			}
+			else if(c2 == 'G') {
+				return 'C';
+			}
+			else if(c2 == 'C') {
+				return 'A';
+			}
+			else {
+				return 'G';
+			}
+		}
+		else if(c1 == 'G') {
+			if(c2 == 'A') {
+				return 'C';
+			}
+			else if(c2 == 'G') {
+				return 'G';
+			}
+			else if(c2 == 'C') {
+				return 'T';
+			}
+			else {
+				return 'A';
+			}
+		}
+		else if(c1 == 'C') {
+			if(c2 == 'A') {
+				return 'A';
+			}
+			else if(c2 == 'G') {
+				return 'T';
+			}
+			else if(c2 == 'C') {
+				return 'C';
+			}
+			else {
+				return 'G';
+			}
+		}
+		else {
+			if(c2 == 'A') {
+				return 'G';
+			}
+			else if(c2 == 'G') {
+				return 'A';
+			}
+			else if(c2 == 'C') {
+				return 'G';
+			}
+			else {
+				return 'T';
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		DNA = sc.next();
+		char first = ' ', second = ' ';
+		second = DNA.charAt(DNA.length() - 1);
+		for(int idx = N - 1; idx >= 0; idx--) {
+			first = DNA.charAt(idx);
+			second = decode(first, second);
+		}
+		System.out.println(second);
+		
+		sc.close();
+	}
+
+}
