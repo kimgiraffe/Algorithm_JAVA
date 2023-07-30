767ca0cb6a1f3301c7e7d5a4843b1bfec79bdb49
kimgiraffe <kimgiraffe@naver.com>
2023-07-30 21:21:01 +0900

    소수 단어

---
+++ src/baekjoon/BOJ_2153.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_2153 {
+
+	private static String str;
+	private static int N = 0;
+	
+	private static void isPrime(int num) {
+		for(int mod = 2; mod <= Math.sqrt(num); mod++) {
+			if(num % mod == 0) {
+				System.out.println("It is not a prime word.");
+				return;
+			}
+		}
+		System.out.println("It is a prime word.");
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		str = sc.next();
+		for(int idx = 0; idx < str.length(); idx++) {
+			if(str.charAt(idx) >= 'a' && str.charAt(idx) <= 'z')
+				N += str.charAt(idx) -'a' + 1;
+			else {
+				N += str.charAt(idx) - 'A' + 27;
+			}
+		}
+		
+		isPrime(N);
+		
+		sc.close();
+	}
+
+}
