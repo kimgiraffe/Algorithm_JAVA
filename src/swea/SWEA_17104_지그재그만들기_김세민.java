dffafa17d1c3b8987e64907f37e3449724e5ff8c
kimgiraffe <kimgiraffe@naver.com>
2023-08-17 10:27:02 +0900

    SWEA_17104_지그재그만들기

--- src/swea/SWEA_17104_지그재그만들기_김세민.java
+++ src/swea/SWEA_17104_지그재그만들기_김세민.java
@@ -1,5 +1,5 @@
 package swea;
-class SWEA_17104 {
+class SWEA_17104_지그재그만들기_김세민 {
 	public static void main(String[] args) throws Exception {
 		for(int i = 0; i <= 9; i++) {
 			for(int j = 1; j <= 10; j++) {
