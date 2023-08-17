fed47f3bbcf3cc793c99bf7e57cd11e7016b3656
kimgiraffe <kimgiraffe@naver.com>
2023-08-17 10:28:46 +0900

    SWEA_17119_2차원배열출력하기

--- src/swea/SWEA_17119_2차원배열출력하기_김세민.java
+++ src/swea/SWEA_17119_2차원배열출력하기_김세민.java
@@ -1,5 +1,5 @@
 package swea;
-class SWEA_17119 {
+class SWEA_17119_2차원배열출력하기_김세민 {
 	public static void main(String[] args) throws Exception {
 		for(int i = 1; i <= 100; i++) {
 			System.out.print(i + " ");
