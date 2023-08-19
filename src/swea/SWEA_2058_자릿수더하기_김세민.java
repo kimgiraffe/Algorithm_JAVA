df8835e5678a1afc00676b7f3d9d92e5bda3cd0c
kimgiraffe <kimgiraffe@naver.com>
2023-08-19 11:06:54 +0900

    SWEA_2058_자릿수더하기

--- src/swea/SWEA_2058_자릿수더하기_김세민.java
+++ src/swea/SWEA_2058_자릿수더하기_김세민.java
@@ -1,17 +1,31 @@
 package swea;
+
 import java.util.Scanner;
 
-class SWEA_2058_자릿수더하기_김세민 {
-	public static void main(String[] args) throws Exception {
+/**
+ * 2058_자릿수더하기
+ * @author semin.kim
+ * 1.하나의 자연수를 입력받는다.
+ * 2.자연수가 0보다 클 때까지 자연수를 10으로 나누고
+ * 3.10으로 나눈 나머지를 자릿수 합에 더한다.
+ * 4.자릿수를 모두 더한 값을 출력한다.
+ */
+
+public class SWEA_2058_자릿수더하기_김세민 {
+	public static int N, sum;
+	
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		int sum = 0;
-		while(N > 0) {
-			sum += N % 10;
-			N /= 10;
+		N = sc.nextInt();
+		while(N > 0) { // N이 0이 되면 중단
+			sum += N % 10; // 자릿수를 더함
+			N /= 10; // 다음 자리로 이동
 		}
-		System.out.println(sum);
+		
+		System.out.println(sum); // 자릿수 합 출력
+		
 		sc.close();
 	}
+
 }
