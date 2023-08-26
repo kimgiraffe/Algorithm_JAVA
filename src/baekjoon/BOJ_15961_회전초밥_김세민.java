9ed5f16ac7c472fe58bca8360b86788826fc21af
kimgiraffe <kimgiraffe@naver.com>
2023-08-26 14:42:04 +0900

    BOJ_15961_회전초밥

--- src/baekjoon/BOJ_15961_회전초밥_김세민.java
+++ src/baekjoon/BOJ_15961_회전초밥_김세민.java
@@ -3,8 +3,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
