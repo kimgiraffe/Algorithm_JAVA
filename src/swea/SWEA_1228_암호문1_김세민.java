768baea8c81a78ed61c0e3186e744910d575a3ad
kimgiraffe <kimgiraffe@naver.com>
2023-08-29 22:09:45 +0900

    SWEA_1228_암호문1

--- src/swea/SWEA_1228_암호문1_김세민.java
+++ src/swea/SWEA_1228_암호문1_김세민.java
@@ -9,8 +9,11 @@ import java.util.StringTokenizer;
 /**
  * SWEA_1228_암호문1
  * 
+ * 이 처리기는 다음과 같이 1개의 기능을 제공한다.
+ * 1. I(삽입) x, y, s : 앞에서부터 x의 위치 바로 다음에 y개의 숫자를 삽입한다. s는 덧붙일 숫자들이다.[ ex) I 3 2 123152 487651 ]
+ * 위의 규칙에 맞게 작성된 명령어를 나열하여 만든 문자열이 주어졌을 때, 암호문을 수정하고, 수정된 결과의 처음 10개 숫자를 출력하는 프로그램을 작성하여라.
  * Arraylist에 원본 암호문을 삽입한다.
- * x의 위치 다음에  y개의 숫자를 삽입한다.
+ * x의 위치 다음에 y개의 숫자를 삽입한다.
  * 다음 삽입되는 위치는 x + 1이다.
  * 
  * @author semin.kim
@@ -25,7 +28,7 @@ public class SWEA_1228_암호문1_김세민 {
 
 	static final int T = 10; // 테스트 케이스의 개수는 10으로 고정
 	static int originalPasswordLength; //원본 암호문의 길이
-	static ArrayList<Integer> password;
+	static ArrayList<Integer> password; // 암호문을 저장할 ArrayList
 	static int commandCount; // 명령어의 개수
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -48,13 +51,13 @@ public class SWEA_1228_암호문1_김세민 {
 
 			st = new StringTokenizer(br.readLine().trim());
 			for(int commandIdx = 0; commandIdx < commandCount; commandIdx++) {
-				char command = st.nextToken().charAt(0);
-				int x = Integer.parseInt(st.nextToken());
-				int numCount = Integer.parseInt(st.nextToken());
+				st.nextToken().charAt(0);
+				int addIdx = Integer.parseInt(st.nextToken()); // 숫자를 삽입할 위치 입력
+				int numCount = Integer.parseInt(st.nextToken()); // 삽입할 숫자의 개수 입력
 				// 입력받은 숫자 삽입
 				for(int numIdx = 0; numIdx < numCount; numIdx++) {
 					int num = Integer.parseInt(st.nextToken());
-					password.add(x++, num);
+					password.add(addIdx++, num); // 숫자 1개를 삽입하면 다음 삽입할 위치는 1 증가
 				}
 			}
 
