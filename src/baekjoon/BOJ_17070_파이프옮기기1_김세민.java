bc1290e54be59acd279d40bd5b58344462e38e45
kimgiraffe <kimgiraffe@naver.com>
2023-08-30 16:41:51 +0900

    BOJ_17070_파이프옮기기1(DP)

--- src/baekjoon/BOJ_17070_파이프옮기기1_김세민.java
+++ src/baekjoon/BOJ_17070_파이프옮기기1_김세민.java
@@ -7,8 +7,12 @@ import java.util.StringTokenizer;
 
 /**
  * BOJ_17070_파이프옮기기1
- * 1.집의 크기와 집의 상태를 입력받는다.
- * 2.깊이 우선 탐색을 통해 파이프를 이동시키는 방법의 수를 구한다.
+ * 현재 칸이 (i, j)이고 현재 파이프가 가로 방향(0)으로 놓여있는 경우... 직전 칸으로 가능한 경우는 (i, j-1, 0), (i-1, j-1, 2)
+ * 현재 칸이 (i, j)이고 현재 파이프가 세로 방향(1)으로 놓여있는 경우... 직전 칸으로 가능한 경우는 (i-1, j, 1), (i-1, j-1, 2)
+ * 현재 칸이 (i, j)이고 현재 파이프가 대삭선 방향(2)으로 놓여있는 경우... 직전 칸으로 가능한 경우는 (i, j-1, 0), (i-1, j, 1), (i-1, j-1, 2)
+ * 
+ * 각 방향에 대해 해당 위치까지 이동시키는 방법의 수를 저장
+ * 
  * @author semin.kim
  */
 
@@ -17,49 +21,67 @@ public class BOJ_17070_파이프옮기기1_김세민 {
 	static BufferedReader br;
 	static StringTokenizer st;
 	static StringBuilder sb;
-	
-	static int house_size; // 집의 크기
+
+	static int houseSize; // 집의 크기
 	static int[][] house; // 집의 상태를 저장할 2차원 배열
-	
-	public static void dfs(int col, int row, int dir) { // col: 세로, row: 가로, dir: 방향(1:가로, 2:세로, 3:대각선)
-		house[col][row]++;
-		if(dir == 1 || dir == 3) { // 가로 방향이거나 대각선 방향인 경우...
-			// 벽이 아니고 범위를 넘지 않는 경우...
-			if(row < house_size && house[col][row + 1] != -1) {
-				dfs(col, row + 1, 1); // 가로 방향으로 이동 
+	static int[][][] dp;
+
+	private static void calculateWaysOfMovingPipe() {
+
+		for(int colIdx = 1; colIdx <= houseSize; colIdx++) {
+			for(int rowIdx = 1; rowIdx <= houseSize; rowIdx++) {
+				if(colIdx == 1 && rowIdx <= 2) continue;
+				if(house[colIdx][rowIdx] == 1) continue;
+
+				/// 가로
+				if(house[colIdx][rowIdx-1] == 1) {
+					dp[colIdx][rowIdx][0] = 0;
+				}
+				else {
+					dp[colIdx][rowIdx][0] = dp[colIdx][rowIdx-1][0] + dp[colIdx][rowIdx-1][2];
+				}
+				// 세로
+				if(house[colIdx-1][rowIdx] == 1) {
+					dp[colIdx][rowIdx][1] = 0;
+				}	
+				else {
+					dp[colIdx][rowIdx][1] = dp[colIdx-1][rowIdx][1] + dp[colIdx-1][rowIdx][2];
+				}
+
+				// 대각선
+				if(house[colIdx-1][rowIdx] == 1 || house[colIdx][rowIdx-1] == 1) {
+					dp[colIdx][rowIdx][2] = 0;
+				}
+				else {
+					dp[colIdx][rowIdx][2] = dp[colIdx-1][rowIdx-1][0] + dp[colIdx-1][rowIdx-1][1] + dp[colIdx-1][rowIdx-1][2];
+				}
 			}
 		}
-		if(dir == 2 || dir == 3) { // 세로 방향이거나 대각선 방향인 경우...
-			// 벽이 아니고 범위를 넘어서지 않는 경우...
-			if(col < house_size && house[col + 1][row] != -1) {
-				dfs(col + 1, row, 2); // 세로 방향으로 이동
-			}
-		}
-		// 벽이 아니고 범위를 넘어서지 않는 경우...
-		if(col < house_size && row < house_size && house[col+1][row] != -1 && house[col][row+1] != -1 && house[col+1][row+1]!=-1) {
-			dfs(col + 1, row + 1, 3); // 대각선 방향으로 이동
-		}
+		System.out.println((dp[houseSize][houseSize][0]+dp[houseSize][houseSize][1]+dp[houseSize][houseSize][2]));
 	}
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		
-		house_size = Integer.parseInt(br.readLine().trim()); // 집의 크기 입력
-		
-		house = new int[house_size+1][house_size+1]; // 집의 번호는 1,1부터 시작
-		
+
+		houseSize = Integer.parseInt(br.readLine().trim()); // 집의 크기 입력
+
+		house = new int[houseSize+1][houseSize+1];
+		dp = new int[houseSize+1][houseSize+1][3];
+
 		// 집의 상태 입력
-		for(int colIdx = 1; colIdx <= house_size; colIdx++) {
+		for(int colIdx = 1; colIdx <= houseSize; colIdx++) {
 			st = new StringTokenizer(br.readLine().trim());
-			for(int rowIdx = 1; rowIdx <= house_size; rowIdx++) {
+			for(int rowIdx = 1; rowIdx <= houseSize; rowIdx++) {
 				house[colIdx][rowIdx] = Integer.parseInt(st.nextToken());
-				if(house[colIdx][rowIdx] == 1) house[colIdx][rowIdx] = -1; // 벽
 			}
 		}
-		// 파이프의 초기상태는 (1,1)과 (1,2)를 차지하고 방향은 가로
-		dfs(1, 2, 1);
 
-		if(house[house_size][house_size] == -1) house[house_size][house_size] = 0;
-		System.out.println(house[house_size][house_size]);
+		// 가장 처음 파이프는 (1,1), (1,2)를 차지하고 있고, 방향은 가로
+		dp[1][2][0] = 1;
+		dp[1][2][1] = 0;
+		dp[1][2][2] = 0;
+		
+		calculateWaysOfMovingPipe();
+
 	}
 }
