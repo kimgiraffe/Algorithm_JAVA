53a385cc8dffa4e18a2d9d864872564d72486376
kimgiraffe <kimgiraffe@naver.com>
2023-09-26 20:48:36 +0900

    BOJ_2239_스도쿠

--- src/baekjoon/BOJ_2239_스도쿠_김세민.java
+++ src/baekjoon/BOJ_2239_스도쿠_김세민.java
@@ -18,9 +18,10 @@ import java.util.StringTokenizer;
  * 9개의 줄에 9개의 숫자로 답을 출력한다. 답이 여러 개 있다면 그 중 사전식으로 앞서는 것을 출력한다. 즉, 81자리의 수가 제일 작은 경우를 출력한다.
  * 
  * [풀이]
- * 맨 위부터 채우기 시작하여 오른쪽으로 한 칸씩 숫자를 채운다.
- * 유효하지 않은 스도쿠인 경우 중단
+ * (0,0)부터 스도쿠를 숫자 1부터 9까지 채워본다.(사전순으로 앞서야 하므로 1부터 채우기)
+ * 유효한 스도쿠인 경우 오른쪽으로 한 칸 이동하여 채운다.
  * 가로로 다 채운 경우 다음 줄 채우기 시작한다.
+ * 모든 칸을 채운 경우 완성된 스도쿠를 출력한다.
  * 
  * @author semin.kim
  *
@@ -39,18 +40,19 @@ public class BOJ_2239_스도쿠_김세민 {
 	static boolean ok;
 	
 	private static boolean isValid(int col, int row, int num) {
+		// 가로 세로에서 이미 존재 숫자가 존재하는 경우...
 		for(int idx = 0; idx < SUDOKU_SIZE; idx++) {
 			if(sudoku[col][idx] == num || sudoku[idx][row] == num) {
 				return false;
 			}
 		}
 		
-		// 3 * 3 크기의 격자의 끝
-		int gridColEdge = (col / 3) * 3;
-		int gridRowEdge = (row / 3) * 3;
+		// 3 * 3 크기의 보드 내에서 중복되는 숫자가 존재하는 경우...
+		int gridCol = (col / 3) * 3;
+		int gridRow = (row / 3) * 3;
 		
-		for(int colIdx = gridColEdge; colIdx < gridColEdge + 3; colIdx++) {
-			for(int rowIdx = gridRowEdge; rowIdx < gridRowEdge + 3; rowIdx++) {
+		for(int colIdx = gridCol; colIdx < gridCol + 3; colIdx++) {
+			for(int rowIdx = gridRow; rowIdx < gridRow + 3; rowIdx++) {
 				if(sudoku[colIdx][rowIdx] == num) {
 					return false;
 				}
@@ -74,17 +76,31 @@ public class BOJ_2239_스도쿠_김세민 {
 				}
 				sb.append("\n");
 			}
-			System.out.println(sb);
+			System.out.print(sb);
+			ok = true;
 			return;
 		}
 		
-		if(sudoku[col][row] > 0) { // 아직 채워지지 않은 칸인 경우...
+		if(sudoku[col][row] > 0) { // 이미 채운 칸인 경우...
 			if(row == 8) { // 가로로 숫자를 다 채운 경우...
 				fill(col + 1, 0); // 다음 줄로 이동하여 채우기
 			}
-			fill(col, row + 1);
-		} else { // 이미 채운 칸인 경우...
-			
+			else { // 오른쪽으로 한 칸 이동하여 채우기
+				fill(col, row + 1);
+			}
+		} else { // 아직 채워지지 않은 칸인 경우...
+			for(int num = 1; num <= SUDOKU_SIZE; num++) {
+				if(isValid(col, row, num)) { // 유효한 스도쿠인 경우...
+					sudoku[col][row] = num; // 숫자를 채우기
+					if(row == 8) { // 가로로 숫자를 다 채운 경우...
+						fill(col + 1, 0); // 다음 줄로 이동하여 채우기
+					}
+					else { // 오른쪽으로 한 칸 이동하여 채우기
+						fill(col, row + 1);
+					}
+					sudoku[col][row] = 0; // 다시 돌려놓기
+				}
+			}
 		}
 		
 	}
@@ -101,8 +117,7 @@ public class BOJ_2239_스도쿠_김세민 {
 				sudoku[row][col] = input.charAt(col) - '0';
 			}
 		}
-		
-		
+		fill(0, 0);
 		
 	}
 }
